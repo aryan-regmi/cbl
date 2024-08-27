371c9200d0985344bc7c4b4b33847fa844bbe2ca
Aryan Regmi <aryan_regmi@protonmail.com>
2024-08-27 15:44:59 -0600

    More dynamic array methods

--- include/cbl/dynamic_array.h
+++ include/cbl/dynamic_array.h
@@ -1,9 +1,11 @@
 #ifndef CBL_DYNAMIC_ARRAY_H
 #define CBL_DYNAMIC_ARRAY_H
 
+#include "cbl/assert.h"
 #include "cbl/mem/allocator.h"
 #include "cbl/primitives.h" // usize
 #include "cbl/slice.h"
+#include <cstring>
 
 namespace cbl {
 
@@ -22,7 +24,15 @@ public:
   static auto
   initWithCapacity(mem::Allocator& allocator,
                    usize           capacity) noexcept -> UnmanagedDynamicArray {
-    // TODO: Allo
+    T* elems = allocator.createArray<T>(capacity);
+    CBL_ASSERT(elems != nullptr, "Allocation failed");
+
+    UnmanagedDynamicArray self;
+    self._elems = elems;
+    self._len   = 0;
+    self._cap   = capacity;
+
+    return self;
   }
 
   /// Creates an array from the given slice.
@@ -34,7 +44,13 @@ public:
   /// The call to `deinit` must use the same allocator that initialized the
   /// slice.
   static auto
-       initFromOwnedSlice(Slice<T> slice) noexcept -> UnmanagedDynamicArray;
+  initFromOwnedSlice(Slice<T> slice) noexcept -> UnmanagedDynamicArray {
+    UnmanagedDynamicArray self;
+    self._elems = slice.ptr();
+    self._len   = slice.len();
+    self._cap   = slice.len();
+    return self;
+  }
 
   /// Frees all memory allocated by the array.
   ///
@@ -42,14 +58,26 @@ public:
   ///
   /// Calling just the destructor will result in a memory leak; `deinit` must be
   /// called to free allocated memory.
-  auto deinit(mem::Allocator allocator) noexcept -> void;
+  auto deinit(mem::Allocator& allocator) noexcept -> void {
+    allocator.deallocate(this->_elems);
+    this->_elems = nullptr;
+    this->_len   = 0;
+    this->_cap   = 0;
+  }
 
   /// Returns the array as a slice that the caller owns.
   ///
   /// # Note
   ///
   /// This will empty the array and clear its capacity.
-  auto toOwnedSlice(mem::Allocator allocator) noexcept -> Slice<T>;
+  auto toOwnedSlice(mem::Allocator& allocator) noexcept -> Slice<T> {
+    const usize len     = this->len;
+    T*          new_mem = allocator.createArray<T>(len);
+    void*       copied  = std::memcpy(new_mem, this->_elems, sizeof(T) * len);
+    CBL_ASSERT(copied != nullptr, "`memcpy` failed");
+    this->deinit(allocator);
+    return Slice<T>{new_mem, len};
+  }
 
   /// Returns the array's elements.
   ///
@@ -61,16 +89,24 @@ public:
   ///
   /// The returned slice will be invalid after any operations that change the
   /// size of the array (such as appends, removes, inserts, etc).
-  auto elems() const noexcept -> Slice<T>;
+  auto elems() const noexcept -> Slice<T> {
+    return Slice<T>{this->_elems, this->_len};
+  }
 
   /// Returns the number of elements in the array.
-  auto len() const noexcept -> usize;
+  auto len() const noexcept -> usize { return this->_len; }
 
   /// Returns the number of elements that the array has reserved memory for.
-  auto cap() const noexcept -> usize;
+  auto cap() const noexcept -> usize { return this->_cap; }
 
   /// Creates a copy of the array.
-  auto clone(mem::Allocator allocator) const noexcept -> UnmanagedDynamicArray;
+  auto
+  clone(mem::Allocator& allocator) const noexcept -> UnmanagedDynamicArray {
+    UnmanagedDynamicArray cloned =
+        UnmanagedDynamicArray::initWithCapacity(allocator, this->_cap);
+    cloned.appendSlice(allocator, this->elems());
+    return cloned;
+  }
 
   /// Inserts `value` at the specified index.
   ///
@@ -84,7 +120,28 @@ public:
   /// # Safety
   ///
   /// Invalidates element pointers if additional memory is needed.
-  auto insert(mem::Allocator allocator, T value, usize idx) noexcept -> void;
+  auto insert(mem::Allocator& allocator, T value, usize idx) noexcept -> void {
+    if (idx == this->_len) {
+      this->append(allocator, value);
+      return;
+    }
+
+    // Resize original buffer if necessary
+    if (this->_len + 1 > this->_cap) {
+      resize();
+    }
+
+    // TODO: Replace with memmove?
+    //
+    // Shift all elements from the idx to the right
+    for (usize i = this->_len; i > idx; i--) {
+      this->_elems[i] = this->_elems[i - 1];
+    }
+
+    // Insert value at `idx`
+    this->_elems[idx]  = value;
+    this->_len        += 1;
+  }
 
   /// Insert `slice` at the specified index.
   ///
@@ -146,6 +203,11 @@ private:
   T*    _elems = nullptr;
   usize _len   = 0;
   usize _cap   = 0;
+
+  auto  resize() -> void {
+    // TODO: Impl!
+    //  - handle initial insert (when _elems == nullptr and _len & _cap are 0)
+  }
 };
 
 } // namespace cbl
